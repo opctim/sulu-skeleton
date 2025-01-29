942c92246d38fca98d29264990db58e052a7923a
Jonathan Hermann <jonathan.hermann@denkwerk.com>
2025-01-29 23:06:37 +0100

    Apply rector and CS fixer

--- application/src/Validator/NotXss.php
+++ application/src/Validator/NotXss.php
@@ -1,11 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Validator;
 
-use Attribute;
 use Symfony\Component\Validator\Constraint;
 
-#[Attribute]
+#[\Attribute]
 class NotXss extends Constraint
 {
     public string $message = 'Die Eingabe ist ungültig.';
